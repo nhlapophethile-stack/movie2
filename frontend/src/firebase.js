a6de37e3a3f2dc5b3d1ecd524b5bf72b87111907
nhlapophethile@gmail.com <nhlapophethile@gmail.com>
2025-11-02 18:18:45 +0200

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,11 +7,12 @@ const firebaseConfig = {
   authDomain: "movies-4180f.firebaseapp.com",
   databaseURL: "https://movies-4180f-default-rtdb.firebaseio.com",
   projectId: "movies-4180f",
-  storageBucket: "movies-4180f.firebasestorage.app",
+  storageBucket: "movies-4180f.appspot.com",
   messagingSenderId: "137906722927",
   appId: "1:137906722927:web:42aadad89e0b746842d59a",
   measurementId: "G-CMT6EC4BTW"
 };
+
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
