48ced5516ec56e151a9af341725156175c9a0454
nhlapophethile@gmail.com <nhlapophethile@gmail.com>
2025-11-02 22:55:29 +0200

    Change API_BASE to production URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // Update API_BASE to point to your backend (e.g. http://localhost:4000)
-export const API_BASE = "http://localhost:4000/api";
+export const API_BASE = "https://movie2-1.onrender.com/api";
 export const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
